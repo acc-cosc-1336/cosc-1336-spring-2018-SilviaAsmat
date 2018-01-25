50c3df6241974b66db5544bd14618c5c49d45092
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2018-01-25 13:35:18 -0600

    Update homework1.py
    Implemented homework assignment

--- src/homework/homework1.py
+++ src/homework/homework1.py
@@ -1,26 +1,41 @@
-def get_hours_since_midnight(seconds):
-    '''
-    Type the code to calculate total hours given n(number) of seconds
-    For example, given 3800 seconds the total hours is 1
-    '''
-    return
+seconds = float(input('Enter number of seconds: '))
 
-'''
-IF YOU ARE OK WITH A GRADE OF 70 FOR THIS ASSIGNMENT STOP HERE.
-'''
+def get_hours_since_midnight (seconds):
+
+    if seconds < 0 or seconds > 86400:
+        return 0
+
+    else:
+        hours_since_midnight = seconds // 3600
+
+    # Type the code to calculate total hours given n(number) of seconds
+    # For example, given 3800 seconds the total hours is 1
+
+        return hours_since_midnight
 
 def get_minutes(seconds):
-    '''
-    Type the code to calculate total minutes less whole hour given n(number) of seconds
-    For example, given 3800 seconds the total minutes is 3
-    '''
 
-    return
+    if seconds < 0 or seconds > 86400 :
+        return 0
+    else:
+
+        minutes = seconds // 60
+        minutes_since_midnight = minutes % 60
+
+    # Type the code to calculate total minutes less whole hour given n(number) of seconds
+    # For example, given 3800 seconds the total minutes is 3
+
+        return minutes_since_midnight
 
 def get_seconds(seconds):
-    '''
-    Type the code to calculate total minutes less whole hour given n(number) of seconds
-    For example, given 3800 seconds the total minutes is 20
-    '''
 
-    return
+    if seconds < 0 or seconds > 86400:
+        return 0
+    else:
+
+        seconds_since_midnight = seconds % 60
+
+    # Type the code to calculate total minutes less whole hour given n(number) of seconds
+    # For example, given 3800 seconds the total minutes is 20
+
+        return seconds_since_midnight
