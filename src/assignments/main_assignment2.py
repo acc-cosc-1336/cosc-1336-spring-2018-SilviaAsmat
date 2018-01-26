4b167dbd752ec2d59f4dde5756e4a3088c164c5f
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2018-01-25 22:25:09 -0600

    Add files via upload

---
+++ src/assignments/main_assignment2.py
@@ -0,0 +1,5 @@
+from assignment2.py import faculty_evaluation_result
+
+# Write code to call the faculty_evaluation_result function with data of your choice
+
+print faculty_evaluation_result (0,5,2,3,8) 
