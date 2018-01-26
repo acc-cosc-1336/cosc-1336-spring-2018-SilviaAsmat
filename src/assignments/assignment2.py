2cbd5e002349ea75e3836ae04b97e39ee80b10d8
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2018-01-26 00:37:57 -0600

    Update assignment2.py

--- src/assignments/assignment2.py
+++ src/assignments/assignment2.py
@@ -1,29 +1,30 @@
 def faculty_evaluation_result(nev, rar, som, oft, voft, alw):
-
-    #Write code to calculate faculty evaluation rating according to asssignment instructions
+    # Write code to calculate faculty evaluation rating according to asssignment instructions
     total = 197
 
-    nev_ratio = nev/total     
-    rar_ratio = rar/total
-    som_ratio = som/total
-    oft_ratio = oft/total
-    voft_ratio = voft/total
-    alw_ratio = alw/total 
- 
+    nev_ratio = nev / total
+    rar_ratio = rar / total
+    som_ratio = som / total
+    oft_ratio = oft / total
+    voft_ratio = voft / total
+    alw_ratio = alw / total
+
     if voft_ratio + alw_ratio >= .9:
         return "Excellent"
-    elif oft_ratio + voft_ratio + alw_ratio >= .8 and oft_ratio + voft_ratio + alw_ratio <.9:
+    elif oft_ratio + voft_ratio + alw_ratio >= .8 and oft_ratio + voft_ratio + alw_ratio < .9:
         return "Very Good"
-    elif som_ratio + oft_ratio + voft_ratio + alw_ratio >= .7 and som_ratio + oft_ratio + voft_ratio + alw_ratio <.8:
+    elif som_ratio + oft_ratio + voft_ratio + alw_ratio >= .7 and som_ratio + oft_ratio + voft_ratio + alw_ratio < .8:
         return "Good"
-    elif rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio >= .6 and rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio <.7:
+    elif rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio >= .6 and rar_ratio + som_ratio + oft_ratio + voft_ratio + alw_ratio < .7:
         return "Needs Improvement"
+    elif nev_ratio <.6:
+        return "Unacceptable"
     else:
-    return "Unacceptable"
+        return "Unacceptable"
 
-def get_ratings(nev,rar,som, oft,voft, alw):
 
-    #Students aren't expected to know this material yet
+def get_ratings(nev, rar, som, oft, voft, alw):
+    # Students aren't expected to know this material yet
 
     ratings = []
     total = nev + rar + som + oft + voft + alw
@@ -36,6 +37,7 @@ def get_ratings(nev,rar,som, oft,voft, alw):
     ratings.append(round(nev / total, 2))
 
     return ratings
+
     
 
     
