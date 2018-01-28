e27423318e036d32ac05f6c8756ddcb4b8466bd1
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2018-01-27 22:16:55 -0600

    Add files via upload

---
+++ src/assignments/main_assignment2 (2).py
@@ -0,0 +1,8 @@
+from assignment2 import faculty_evaluation_result
+
+#Write code to call the faculty_evaluation_result function with data of your choice
+print("Using numbers 10, 20, 163, 32, 64, 19")
+result = faculty_evaluation_result(10, 20, 163, 32, 64, 19)
+print("The rating was:")
+print(result)
+
