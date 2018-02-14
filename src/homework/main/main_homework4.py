96b6d0f72e203a0d30a535976549a795a90518d7
SilviaAsmat <silvia.asmat@g.austincc.edu>
2018-02-13 18:23:28 -0600

    Downloading HW 4

---
+++ src/homework/main/main_homework4.py
@@ -0,0 +1,26 @@
+#write the import statements to bring in homework 4 functions
+#valid_letter_grade, get_credit_points, get_grade_points, and get_grade_point_average
+
+'''
+Use the functions from homework4 to...
+Write code to prompt the user for number of students and grades.
+Create a nested loop to collect letter grades and credit hours for each student.
+Vaidate the letter grade is in accepted range if not prompt user for letter again.
+From the letter grade, get the credit points for that class.
+Calculate grade points (How? Research GPA calculation).
+Sum grade points and credit hours for each student.
+Calculate GPA for each student.
+Display Student 1(etc) GPA is 3.77. Format the GPA to two values.
+'''
+
+#WRITE YOUR CODE IN THE MAIN FUNCTION BELOW
+def main():
+
+
+
+
+
+
+#CALL THE MAIN FUNCTION
+
+
