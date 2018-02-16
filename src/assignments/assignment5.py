551e817a9c0083242c6b8499569844da81012e01
SilviaAsmat <silvia.asmat@g.austincc.edu>
2018-02-15 20:44:01 -0600

    Downloading Assignment 5

---
+++ src/assignments/assignment5.py
@@ -0,0 +1,9 @@
+def recursive_decimal_binary(num, bits):
+    '''
+    Given a whole number from the decimal system, converts the number to 8-bit binary
+    Given 86 returns 01010110
+    :param num:
+    :return:
+    '''
+
+
---
+++ tests/assignments/test_assign5.py
@@ -0,0 +1,13 @@
+import unittest
+
+#write import for decimal to binary function
+
+
+class Test_Assign4(unittest.TestCase):
+
+    def test_sample_one(self):
+        self.assertEqual(1,1)
+
+    #write test cases with arguments 85 and 63 for recursive_decimal_binary function
+
+
