5bb156a6837e06b37961c07adfddd5fb45cf2c01
SilviaAsmat <silvia.asmat@g.austincc.edu>
2018-02-20 17:59:00 -0600

    Created Functions Hw5

--- src/homework/homework5.py
+++ src/homework/homework5.py
@@ -1,7 +1,15 @@
-
 #Create a function named write_sales_data with file_object, item and price as parameters.
 #The function should write item and price to a file.
+def write_sales_data(file_object,item,price):
+    myfile = open(file_object,'a')
+    myfile.write(item + ' ')
+    myfile.write(price + '\n')
+    myfile.close()
+
+
+
 
 #Create another function named read_sales_data with file_object as a parameter.
 #The function will read the file line by line and display to screen to produce the table described in homework 5.
+def read_sales_data(file_object):
 
--- src/homework/main/main_homework5.py
+++ src/homework/main/main_homework5.py
@@ -1,5 +1,8 @@
 #write the import statements for homework5 functions
 
+from src.homework.homework5 import write_sales_data
+from src.homework.homework5 import read_sales_data
+
 #With the functions created in homework5...
 #Prompt user for number of sales records to input.
 #Open a file for text writing.
@@ -7,6 +10,15 @@
 #Save item name and price to file in one line
 #Calculate sum of price and write to file
 #Calculate average price and write to file
+def main():
+    sales_records = int(input('Enter number of sales records: '))
+
+    item_name = input('Enter item name')
+    item_price = int(input('Enter item price'))
+
+
+
+
 
 #Open a file for text reading.
 #Read the saved file and output table
