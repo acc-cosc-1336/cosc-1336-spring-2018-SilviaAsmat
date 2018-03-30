0b229999064f20c1f1cf4b66bbb69269a4a7f671
SilviaAsmat <silvia.asmat@g.austincc.edu>
2018-03-29 22:13:29 -0500

    Fixing test_assign9 last test case

---
+++ src/assignments/main.py
@@ -0,0 +1,31 @@
+from src.assignments.invoice_item import InvoiceItem
+from src.assignments.invoice import Invoice
+#Write import statements for classes invoice and invoice_item
+'''
+LOOK AT THE TEST CASES FOR HINTS
+Create an invoice object
+In the loop:
+Create a new InvoiceItem
+Create a user controlled loop to continue until y is not typed, in loop...
+    Prompt user for description, quantity, and cost.
+    Add values to the InvoiceItem.
+    Add the InvoiceItem to the invoice object.
+    Once user types a letter other than y display the Invoice to screen
+'''
+
+
+def main():
+    invoice_obj = []
+    keep_going = 'y'
+    while keep_going == 'y':
+        invoice_item = InvoiceItem()
+        description = input("Enter description: ")
+        quantity = input("Enter quantity: ")
+        cost = input("Enter cost: ")
+
+        invoice_obj.append(invoice_item)
+        keep_going = input("Enter y to continue: ")
+    print(invoice_obj)
+
+
+main()
---
+++ tests/assignments/test_assign9.py
@@ -0,0 +1,47 @@
+import unittest
+from src.assignments.invoice import Invoice
+from src.assignments.invoice_item import InvoiceItem
+
+
+class Test_Assign8(unittest.TestCase):
+
+    invoice_items = [] #list of Invoice Item instance objects
+
+    def test_invoice_item_extended_cost_w_qty_10_cost_5(self):
+        '''
+        Create an Invoice item instance with argument values: 'Widget1', 10, and 5
+        The extended cost result should be 50;
+
+        '''
+        invoice_item = InvoiceItem('Widget1', 10, 5)
+        extended_cost = invoice_item.get_extended_cost()
+
+        self.assertEqual(50, extended_cost)
+        #create the assert code
+
+    def test_invoice_w_3_invoice_items(self):
+        '''
+        Create an Invoice instance with argument values: 'ABC company', '03282018'
+        Three invoice items as follows argument values:
+        'Widget1', 10, and  5
+        'Widget2',  7, and  8
+        'Widget3', 20, and 10
+        Get Extended result  should be 50 + 56 + 200 = 306
+        '''
+        invoice = Invoice('ABC Company', '03282018')
+
+        invoice.add_invoice_item(InvoiceItem('Widget1', 10, 5))
+        invoice.add_invoice_item(InvoiceItem('Widget2', 7, 8))
+        invoice.add_invoice_item(InvoiceItem('Widget3', 20, 10))
+
+        invoice_item = InvoiceItem('Widget1', 10, 5)
+        cost1 = invoice_item.get_extended_cost()
+        invoice_item = InvoiceItem('Widget2', 7, 8)
+        cost2 = invoice_item.get_extended_cost()
+        invoice_item = InvoiceItem('Widget3', 20, 10)
+        cost3 = invoice_item.get_extended_cost()
+
+        result = cost1 + cost2 + cost3
+        #create the assert equal code
+        self.assertEqual(306, result)
+
