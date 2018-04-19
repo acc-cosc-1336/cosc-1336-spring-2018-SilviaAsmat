098922efcfbc015ee0636a6f780c3384da7936f6
SilviaAsmat <silvia.asmat@g.austincc.edu>
2018-04-18 21:11:26 -0500

    fixing hw11 die class

--- src/homework/homework11/player.py
+++ src/homework/homework11/player.py
@@ -6,8 +6,8 @@ class Player:
 
     #ASSIGNMENT 12: modify the constructor parameter list and add die1 and die2 as parameters.
     def __init__(self, game_log, die1, die2):
-        self.die1 = Die(6) #ASSIGNMENT12: update the statement to use the die1 parameter argument
-        self.die2 = Die(8) #ASSIGNMENT12: update the statement to use the die2 parameter argument
+        self.die1 = die1 #ASSIGNMENT12: update the statement to use the die1 parameter argument
+        self.die2 = die2 #ASSIGNMENT12: update the statement to use the die2 parameter argument
         self.game_log = game_log
 
     def roll_doubles(self):
