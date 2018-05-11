26b45a796c8eea867b4a530f7fb0e22563d414ad
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 15:58:23 -0500

    final grading
    -1 calculate doesn't require parameters
    -1 employee id and name assignment done in Employee

--- src/final_exam/q_employee/salaried_employee.py
+++ src/final_exam/q_employee/salaried_employee.py
@@ -1,13 +1,10 @@
-from src.final_exam.employee import Employee
-
+from src.final_exam.q_employee.employee import Employee
 
 class SalariedEmployee(Employee):
 
     def __init__(self, yearly_rate, employee_id, name):
         Employee.__init__(self, employee_id, name)
         self.yearly_rate = yearly_rate
-        self.employee_id = employee_id
-        self.name = name
-
-    def calculate_biweekly_pay(self, yearly_rate):
-        return yearly_rate/26
+        
+    def calculate(self):
+        return self.yearly_rate/26
