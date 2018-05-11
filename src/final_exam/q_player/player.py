252385b3f4d863c6445ce17fbceffd487d0d2ec5
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 18:55:00 -0500

    final grading
    -2 if logic for dice ranges incorrect
    -1 b=True and b=False should use the == sign

--- src/final_exam/q_player/player.py
+++ src/final_exam/q_player/player.py
@@ -2,15 +2,15 @@ class Player:
 
     def check_come_out_roll(self, d1, d2,b=True):
         sum = d1+d2
-        if 6< d1 <0 or 6< d2 <0:
+        if d1 > 0 or  d1 > 6 and  d2 < 0 or d2 > 6:
             return 'Invalid range'
-        elif sum == 7 or sum == 11 and b=True:
+        elif sum == 7 or sum == 11 and b==True:
             return 'Winner'
-        elif sum == 7 or sum == 11 and b=False:
+        elif sum == 7 or sum == 11 and b==False:
             return 'Loser'
-        elif sum == 2 or sum == 3 or sum == 12 and b=True:
+        elif sum == 2 or sum == 3 or sum == 12 and b==True:
             return 'Loser'
-        elif sum == 2 or sum == 3 or sum == 12 and b=False:
+        elif sum == 2 or sum == 3 or sum == 12 and b==False:
             return 'Winner'
         else:
             return sum
