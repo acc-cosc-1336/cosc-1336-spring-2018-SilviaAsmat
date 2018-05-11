4f6d7955b72b5224688173435d22a81175db5fc0
Art Gonzalez <artgonzalezacc@users.noreply.github.com>
2018-05-11 17:53:53 -0500

    blackboard submission

---
+++ src/final_exam/q_pie/main_pie.py
@@ -0,0 +1,10 @@
+from src.final_exam.Pie_Arc import Pie_Arc
+from src.final_exam.PieChart import PieChart
+
+pie_arc = Pie_Arc
+pie_arcs = [pie_arc('one'),pie_arc('two'),pie_arc('three'),pie_arc('four')]
+
+pie_chart = PieChart
+pie_chart(pie_arcs)
+
+pie_chart()
